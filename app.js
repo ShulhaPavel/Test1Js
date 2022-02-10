e5068d3f4b3f2c65529ecdd921a83430b7958363
ShulhaPavel <63409432+ShulhaPavel@users.noreply.github.com>
2022-02-10 13:19:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello Word");
+const name = "Pavel";
+let age = 24;
+let boo = true;
+
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".nav_link");
+const h1 = document.querySelectorAll (".h1");
+
+
+console.log(navLinks);
+console.log(header, navLinks);
+console.log(h1);
+
+window.addEventListener("scroll", function(){
+    header.classList.add('blue');
+});
+
+window.addEventListener("scroll", function(){
+    [...navLinks].map((link) => link.classList.add('red'));
+});
+
+window.addEventListener("scroll", function(){
+    [...h1].map((link) => link.classList.add('green'));
+});
+
